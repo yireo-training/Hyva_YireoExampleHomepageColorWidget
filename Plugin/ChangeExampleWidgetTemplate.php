c0dd9eb486299384cd2989ee692786aafa5dd04a
Jisse Reitsma <jisse@yireo.com>
2021-10-04 14:37:00 +0200

    Formatting

--- Plugin/ChangeExampleWidgetTemplate.php
+++ Plugin/ChangeExampleWidgetTemplate.php
@@ -6,7 +6,6 @@ use Yireo\ExampleHomepageColorWidget\Block\ExampleWidget;
 
 class ChangeExampleWidgetTemplate
 {
-    // @todo: Register this plugin through di.xml
     public function beforeToHtml(ExampleWidget $block)
     {
         $block->setTemplate('Hyva_YireoExampleHomepageColorWidget::example.phtml');
